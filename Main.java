aa7dcde029f4f193842c4948408cd77ecfad4b0b
P4UL0Jr <127964717+P4UL0Jr@users.noreply.github.com>
2024-04-30 12:09:18 -0300

    tratando erros e excessões

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
         Conversor c1 = new Conversor();
         int escolha = 0;
@@ -18,24 +19,39 @@ public class Main {
                 "7.  --SAIR--"
         };
         while (escolha!=7) {
-
-            System.out.print("Menu Principal");
-
-            for (int i = 0; i < menu.length; i++) {
-                System.out.print(menu[i] + "\n");
-            }
-
-            System.out.print("\nDigite sua escolha: ");
-            escolha = sc.nextInt();
-            System.out.println("\nDigite a quantidade de BRL que você quer converter");
-            double qnt = sc.nextDouble();
             try {
-                System.out.printf("%.2f",c1.conversorDeMoeda(escolha, qnt));
-                System.out.println("");
+                System.out.print("Menu Principal");
+
+                for (int i = 0; i < menu.length; i++) {
+                    System.out.print(menu[i] + "\n");
+                }
+
+                System.out.print("\nDigite sua escolha: ");
+                escolha = sc.nextInt();
+                if (escolha != 7&& escolha < 7) {
+                    System.out.println("\nDigite a quantidade de BRL que você quer converter");
+                    double qnt = sc.nextDouble();
+
+                    System.out.printf("%.2f", c1.conversorDeMoeda(escolha, qnt));
+                    System.out.println("");
+                    Thread.sleep(3000);
+                }if(escolha>7){
+                    System.out.println("Digite uma opção válida");
+                    Thread.sleep(3000);
+                }
+            }catch (InterruptedException | InputMismatchException e) {
+
+                System.out.println("Digite uma opção válida");
                 Thread.sleep(3000);
-            } catch (InterruptedException e) {
+                sc.nextLine();
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+
         }
+
+        System.out.println("Saindo...");
+        sc.close();
     }
+
 }
